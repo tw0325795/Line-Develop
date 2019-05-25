7109b00eab111e65b748a5e725385daa9ce851db
Alan <tw0325795@gmail.com>
2019-05-26 02:28:48 +0800

    安裝Line-bot-sdk

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\DevelopLog;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 class LineController extends Controller
 {
     /**
@@ -17,8 +20,20 @@ class LineController extends Controller
     }
 
     public function getMessage(Request $request){
+        $httpClient = new CurlHTTPClient(env('LINE_CHANNEL_ACCESS_TOKEN'));
+        $bot = new LINEBot($httpClient, ['channelSecret' => env('LINE_SECRET')]);
+
+        $textMessageBuilder = new TextMessageBuilder('伺服器收到:'.$request->event[0]->type);
+        $response = $bot->replyMessage($request->event[0]->replyToken, $textMessageBuilder);
+        if ($response->isSucceeded()) {
+            $data = 'Succeeded!';
+        }else{
+            // Failed
+            $data = $response->getHTTPStatus() . ' ' . $response->getRawBody();
+        }
+
         DevelopLog::create([
-            'data'=>json_encode($request->all())
+            'data'=>json_encode($data)
         ]);
         return [];
     }
